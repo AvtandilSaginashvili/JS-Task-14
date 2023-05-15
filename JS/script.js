ae4f268553bf3bc82e1e4a8ffe5e6caef4132007
ავთანდილ საგინაშვილი <120257059+AvtandilSaginashvili@users.noreply.github.com>
2023-05-15 13:25:25 +0400

    update

---
+++ JS/script.js
@@ -0,0 +1,13 @@
+
+
+function shecvale() {
+
+    var blue = document.getElementById("ball");
+    var green = document.getElementById("gball");
+
+    blue.style.transitionDelay = "0.5s";
+    blue.style.visibility = "hidden";
+    green.style.transitionDelay = "1s";
+    green.style.visibility = "visible"
+    
+}
